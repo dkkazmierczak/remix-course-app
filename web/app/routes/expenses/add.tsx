677f1fbb55aeef6d73ec4145cbb43b2bcfe2d4b3
Dominika Kazmierczak <dominika@enabler.no>
2023-10-05 14:10:15 +0200

    Refactor

--- web/app/routes/expenses/add.tsx
+++ web/app/routes/expenses/add.tsx
@@ -1,7 +1,5 @@
+import ExpenseForm from '~/components/expenses/ExpenseForm';
+
 export default function addExpensesPage() {
-  return (
-    <div>
-      <h1>Add Expenses Page</h1>
-    </div>
-  );
+  return <ExpenseForm />;
 }
