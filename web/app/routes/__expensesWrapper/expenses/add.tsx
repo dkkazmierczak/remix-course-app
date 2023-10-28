db9e97a20e1683d17d59335c4b2872a3c9bc2258
Dominika Kazmierczak <dominika@enabler.no>
2023-10-28 13:46:32 +0200

    Adding Expenses to the database via action()

--- web/app/routes/__expensesWrapper/expenses/add.tsx
+++ web/app/routes/__expensesWrapper/expenses/add.tsx
@@ -1,6 +1,8 @@
 import ExpenseForm from '~/components/expenses/ExpenseForm';
-import Modal from '../../../components/util/Modal';
+import Modal from '~/components/util/Modal';
 import { useNavigate } from '@remix-run/react';
+import { addExpense } from '~/data/expenses.server';
+import { redirect } from '@remix-run/node';
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -14,4 +16,14 @@ export default function AddExpensesPage() {
       <ExpenseForm />
     </Modal>
   );
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  // formData.get('title');
+  const expenseData = Object.fromEntries(formData);
+  console.log(expenseData, formData);
+
+  await addExpense(expenseData);
+  return redirect('/expenses');
 }
