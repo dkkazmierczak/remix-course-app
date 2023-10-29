2193b3295246298e091b7945b721cc5a6d9f4787
Dominika Kazmierczak <dominika@enabler.no>
2023-10-29 09:54:51 +0100

    Updating expense

--- web/app/routes/__expensesWrapper/expenses/$id.tsx
+++ web/app/routes/__expensesWrapper/expenses/$id.tsx
@@ -1,6 +1,9 @@
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '../../../components/util/Modal';
 import { useNavigate } from '@remix-run/react';
+import { updateExpense } from '~/data/expenses.server';
+import { type LoaderArgs, redirect } from '@remix-run/node';
+import { validateExpenseInput } from '~/data/validation.server';
 // import { type LoaderArgs } from '@remix-run/node';
 // import { getExpense } from '~/data/expenses.server';
 
@@ -23,3 +26,18 @@ export default function UpdateExpensesPage() {
 //   const expense = await getExpense(expenseId);
 //   return expense;
 // }
+
+export async function action({ request, params }: LoaderArgs) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  return redirect('/expenses');
+}
