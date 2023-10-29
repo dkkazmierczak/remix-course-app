6b988f4906a011162407c5928b0749471d386e59
Dominika Kazmierczak <dominika@enabler.no>
2023-10-29 13:22:54 +0100

    Session and cookies preparation

--- web/app/data/auth.server.ts
+++ web/app/data/auth.server.ts
@@ -1,5 +1,18 @@
 import { hash, compare } from 'bcryptjs';
 import { prisma } from './database.server';
+import { createCookieSessionStorage } from '@remix-run/node';
+
+const SESSION_SECRET = process.env.SESSION_SECRET!;
+
+const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    secrets: [SESSION_SECRET],
+    sameSite: 'lax',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    httpOnly: true,
+  },
+});
 
 type signupProps = {
   email: string;
